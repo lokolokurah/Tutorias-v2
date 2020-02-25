e8e5f4c279bc1b2d9da695bed0dbdbce2cc6d4a2
Jaime <Jaime@HYRULE-5500X>
2020-02-25 16:46:06 +0100

    10. Modificada Clase MainApp.

--- src/main/java/org/iesalandalus/programacion/tutorias/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/tutorias/MainApp.java
@@ -1,7 +1,9 @@
 package org.iesalandalus.programacion.tutorias;
 
 import org.iesalandalus.programacion.tutorias.mvc.controlador.Controlador;
+import org.iesalandalus.programacion.tutorias.mvc.modelo.IModelo;
 import org.iesalandalus.programacion.tutorias.mvc.modelo.Modelo;
+import org.iesalandalus.programacion.tutorias.mvc.modelo.negocio.FactoriaFuenteDatos;
 import org.iesalandalus.programacion.tutorias.mvc.vista.Vista;
 
 public class MainApp {
@@ -10,7 +12,7 @@ public class MainApp {
 	 * Author: Jaime Ruíz Ramírez
 	 */
 	public static void main(String[] args) {
-		Modelo modelo = new Modelo();
+		IModelo modelo = new Modelo(FactoriaFuenteDatos.MEMORIA.crear());
 		Vista vista = new Vista();
 		Controlador controlador = new Controlador(modelo, vista);
 		controlador.comenzar();
