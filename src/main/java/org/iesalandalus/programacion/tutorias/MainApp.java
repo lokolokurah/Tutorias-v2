484f1a9740c094e313f77a067c490d21afb6dfe1
Jaime <Jaime@HYRULE-5500X>
2020-02-25 16:02:04 +0100

    1. fork + modificación README.md + Import src/main/java/... Tutorias-v1

--- src/main/java/org/iesalandalus/programacion/tutorias/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/tutorias/MainApp.java
@@ -1,9 +1,19 @@
 package org.iesalandalus.programacion.tutorias;
 
+import org.iesalandalus.programacion.tutorias.mvc.controlador.Controlador;
+import org.iesalandalus.programacion.tutorias.mvc.modelo.Modelo;
+import org.iesalandalus.programacion.tutorias.mvc.vista.Vista;
+
 public class MainApp {
-
+	
+	/*
+	 * Author: Jaime Ruíz Ramírez
+	 */
 	public static void main(String[] args) {
-		System.out.println("Gestión de tutorías del IES Al-Ándalus");
+		Modelo modelo = new Modelo();
+		Vista vista = new Vista();
+		Controlador controlador = new Controlador(modelo, vista);
+		controlador.comenzar();
 	}
-
+	
 }
---
+++ src/main/java/org/iesalandalus/programacion/tutorias/mvc/modelo/negocio/Profesores.java
@@ -0,0 +1,87 @@
+package org.iesalandalus.programacion.tutorias.mvc.modelo.negocio;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import javax.naming.OperationNotSupportedException;
+
+import org.iesalandalus.programacion.tutorias.mvc.modelo.dominio.Profesor;
+
+public class Profesores {
+
+	private List<Profesor> coleccionProfesores;
+	
+	public Profesores() 
+	{
+		coleccionProfesores = new ArrayList<>();
+	}
+	
+	public List<Profesor> get() 
+	{
+		List<Profesor> profesoresOrdenados = copiaProfundaProfesores();
+		profesoresOrdenados.sort(Comparator.comparing(Profesor::getDni));
+		return profesoresOrdenados;
+	}
+	
+	private List<Profesor> copiaProfundaProfesores()
+	{
+		List<Profesor> copiaProfesores = new ArrayList<>();
+		for (Profesor profesor : coleccionProfesores) 
+		{
+			copiaProfesores.add(new Profesor(profesor));
+		}
+		return copiaProfesores;
+	}
+	
+	public int getTamano() 
+	{
+		return coleccionProfesores.size();
+	}
+	
+	public void insertar(Profesor profesor) throws OperationNotSupportedException
+	{
+		if (profesor == null) 
+		{
+			throw new NullPointerException("ERROR: No se puede insertar un profesor nulo.");
+		}
+		int indice = coleccionProfesores.indexOf(profesor);
+		if (indice == -1) 
+		{
+			coleccionProfesores.add(new Profesor(profesor));
+		} else {
+			throw new OperationNotSupportedException("ERROR: Ya existe un profesor con ese DNI.");
+		}		
+		
+	}
+
+	public Profesor buscar(Profesor profesor) 
+	{
+		if (profesor == null) 
+		{
+			throw new IllegalArgumentException("ERROR: No se puede buscar un profesor nulo.");
+		}
+		int indice = coleccionProfesores.indexOf(profesor);
+		if (indice == -1) 
+		{
+			return null;
+		} else {
+			return new Profesor(coleccionProfesores.get(indice));
+		}
+	}
+	
+	public void borrar(Profesor profesor) throws OperationNotSupportedException 
+	{
+		if (profesor == null) 
+		{
+			throw new IllegalArgumentException("ERROR: No se puede borrar un profesor nulo.");
+		}
+		int indice = coleccionProfesores.indexOf(profesor);
+		if (indice == -1) 
+		{
+			throw new OperationNotSupportedException("ERROR: No existe ningún profesor con ese DNI.");
+		} else {
+			coleccionProfesores.remove(indice);
+		}
+	}
+}
